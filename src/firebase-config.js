8bec10c9a59b922706167464cddfa6c28d51856a
Daniel Murphy <danielmurphy@Daniels-Mac-mini.local>
2023-03-17 00:00:58 +1300

    initial commit, firebase setup

---
+++ src/firebase-config.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyAFwiWzrjJAruqRHhSwIXzw2fnVV78yHxc",
+
+  authDomain: "hologramqi.firebaseapp.com",
+
+  projectId: "hologramqi",
+
+  storageBucket: "hologramqi.appspot.com",
+
+  messagingSenderId: "956292624586",
+
+  appId: "1:956292624586:web:85d43ef8c451217fd40433"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
